4bef888fe53794dd1594034ce671f903619253f2
Graeme Zinck <graeme.zinck@gmail.com>
2021-11-15 14:24:39 -0500

    Add firebase hosting

--- src/client/ClientAPI.ts
+++ src/client/ClientAPI.ts
@@ -4,6 +4,8 @@ import AudioAPI from './AudioAPI';
 import ClockAPI from './ClockAPI';
 import SessionAPI from './SessionAPI';
 
+const URL = 'ws://localhost:2000';
+
 class ClientAPI {
   public readonly audio: AudioAPI;
   public readonly sessionID: string;
@@ -13,7 +15,7 @@ class ClientAPI {
   public isActive = false;
 
   constructor(ctx: AudioContext, sessionID: string) {
-    const socket = io(process.env.REACT_APP_SERVER_URL as string);
+    const socket = io(process.env.REACT_APP_SERVER_URL || URL);
     this.sessionID = sessionID;
     this.audio = new AudioAPI(socket, sessionID);
     this.session = new SessionAPI(socket, sessionID);
